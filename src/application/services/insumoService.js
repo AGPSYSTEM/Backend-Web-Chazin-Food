7105f40206adfeb8654844e103a0902a27fd6e6f
AGPSYSTEM <gomezpavas34@gmail.com>
2026-08-10 18:39:18 -0500

    fix: softDelete mantiene la ficha tecnica intacta y solo hardDelete la elimina

--- src/application/services/insumoService.js
+++ src/application/services/insumoService.js
@@ -167,13 +167,6 @@ class InsumoService {
     insumo.estado = 0;
     await insumo.save();
 
-    // Delete associated ficha técnica and its details
-    const ficha = await FichaTecnica.findOne({ where: { idInsumo } });
-    if (ficha) {
-      await DetalleFichaInsumo.destroy({ where: { idFichaTecnica: ficha.idFichaTecnica } });
-      await ficha.destroy();
-    }
-
     const { resetAutoIncrement } = require('../../infrastructure/utils/dbUtils');
     await resetAutoIncrement('insumo', 'idInsumo');
     return { message: 'Insumo movido a la papelera' };
